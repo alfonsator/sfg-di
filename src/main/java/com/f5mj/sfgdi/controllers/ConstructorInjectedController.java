8fa90ba5972cb0fe6cd69ae04f4fed49342080e4
alfonsator <alfoglez@icloud.com>
2022-11-27 12:21:42 +0100

    Dependency Injection in Spring Add

--- src/main/java/com/f5mj/sfgdi/controllers/ConstructorInjectedController.java
+++ src/main/java/com/f5mj/sfgdi/controllers/ConstructorInjectedController.java
@@ -1,7 +1,10 @@
 package com.f5mj.sfgdi.controllers;
 
+import org.springframework.stereotype.Controller;
+
 import com.f5mj.sfgdi.services.GreetingService;
 
+@Controller
 public class ConstructorInjectedController {
 
    private final GreetingService greetingService;
--- src/main/java/com/f5mj/sfgdi/controllers/PropertyInjectedController.java
+++ src/main/java/com/f5mj/sfgdi/controllers/PropertyInjectedController.java
@@ -1,9 +1,14 @@
 package com.f5mj.sfgdi.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
 import com.f5mj.sfgdi.services.GreetingService;
 
+@Controller
 public class PropertyInjectedController {
 
+   @Autowired
    public GreetingService greetingService;
 
    public String getGreeting() {
