5df49b25ddaacba697ac954941af304c20a202c6
alfonsator <64138333+alfonsator@users.noreply.github.com>
2023-01-24 19:09:52 +0100

    Beans Scope lesson

--- src/main/java/com/f5mj/sfgdi/SfgDiApplication.java
+++ src/main/java/com/f5mj/sfgdi/SfgDiApplication.java
@@ -9,6 +9,8 @@ import com.f5mj.sfgdi.controllers.I18nController;
 import com.f5mj.sfgdi.controllers.MyController;
 import com.f5mj.sfgdi.controllers.PropertyInjectedController;
 import com.f5mj.sfgdi.controllers.SetterInjectedController;
+import com.f5mj.sfgdi.services.PrototypeBean;
+import com.f5mj.sfgdi.services.SingletonBean;
 
 // To override default component scan (in case we add packages outside main tree defined
 // by main class), add this annotation:
@@ -42,6 +44,18 @@ public class SfgDiApplication {
 		ConstructorInjectedController constructorInjectedController = (ConstructorInjectedController) ctx
 				.getBean("constructorInjectedController");
 		System.out.println(constructorInjectedController.getGreeting());
+
+		System.out.println("-------------- Bean Scopes -------------");
+		SingletonBean singletonBean1 = ctx.getBean(SingletonBean.class);
+		System.out.println(singletonBean1.getMyScope());
+		SingletonBean singletonBean2 = ctx.getBean(SingletonBean.class);
+		System.out.println(singletonBean2.getMyScope());
+
+		PrototypeBean prototypeBean1 = ctx.getBean(PrototypeBean.class);
+		System.out.println(prototypeBean1.getMyScope());
+		PrototypeBean prototypeBean2 = ctx.getBean(PrototypeBean.class);
+		System.out.println(prototypeBean2.getMyScope());
+
 	}
 
 }
